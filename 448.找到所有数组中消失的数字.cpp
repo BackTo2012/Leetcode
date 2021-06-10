7ae77cc5f633b7e773d6e4579d75447e9fec1b1b
BackTo2012 <1742331636@qq.com>
2021-06-10 09:36:25 +0800

    C++

---
+++ 448.找到所有数组中消失的数字.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=448 lang=cpp
+ *
+ * [448] 找到所有数组中消失的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        // for (vector<int>::iterator iter = nums.begin(); iter != nums.end(); iter++)
+        for ( const int & num : nums)
+        {
+            int pos = abs(num) - 1;
+            if (nums[pos] > 0 )
+            {
+                nums[pos] = -nums[pos];
+            }
+        }
+        for (int i=0; i<nums.size();++i){
+            if (nums[i]>0)
+            {
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
